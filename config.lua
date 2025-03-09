3dc8d04c746744468a556c707c09042fef6d69a2
DenyyTyz <denynurcahyanto1@gmail.com>
2025-03-09 18:06:33 +0700

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,16 @@
+Config = {}
+
+-- Framework Configuration
+Config.Framework = 'ESX' -- Pilihan: 'ESX', 'QBCore', 'Standalone'
+
+-- Clothing System
+Config.ClothingSystem = 'illenium' -- Pilihan: 'esx_skin', 'qb-clothing', 'illenium'
+
+-- Target System
+Config.TargetSystem = 'ox_target' -- Pilihan: 'ox_target', 'qb-target'
+
+-- Inventory System
+Config.InventorySystem = 'ox_inventory' -- Pilihan: 'ox_inventory', 'qb-inventory', 'other'
+
+-- Pengaturan Carry
+Config.AllowCarry = true
