d8345cd8c9488ff1c98b7d2dd6d1d03af7ee60a8
DenyyTyz <denynurcahyanto1@gmail.com>
2025-03-09 18:07:48 +0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+autor 'DenyyTyz'
+description 'Players Sleep on Disconnect'
+
+shared_scripts {
+    'config.lua'
+}
+
+server_scripts {
+    'server/*.lua',
+    'modules/*.lua'
+}
+
+client_scripts {
+    'client/*.lua',
+    'modules/*.lua'
+}
